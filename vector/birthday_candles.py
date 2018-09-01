1005d520001e663e7659e08f0c0b692cf8c1b8f6
Suma Dodmani <sumasdodmani@gmail.com>
2018-09-01 13:05:25 -0600

    Create birthday_candles.py

---
+++ vector/birthday_candles.py
@@ -0,0 +1,32 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the birthdayCakeCandles function below.
+def birthdayCakeCandles(ar):
+    max = ar[0]
+    count = 0
+    for i in ar:
+        if (max<i):
+            max = i
+    for j in ar:
+        if(max==j):
+            count += 1
+    return count
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    ar_count = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
