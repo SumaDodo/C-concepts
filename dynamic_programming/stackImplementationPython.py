ccbee7038b0f917f0183185dbcea93621f022969
Suma Dodmani <sumasdodmani@gmail.com>
2018-10-26 01:10:17 -0600

    Create stackImplementationPython.py

---
+++ dynamic_programming/stackImplementationPython.py
@@ -0,0 +1,18 @@
+class Stack:
+    def __init__(self):
+        self.stack = []
+
+    def isEmpty(self):
+        return self.stack == []
+
+    def push(self):
+        return self.stack.append(stack)
+    
+    def pop(self):
+        return self.stack.pop()
+    
+    def peek(self):
+        return self.stack[len(stack)-1]
+    
+    def size(self):
+        return len(self.stack)
