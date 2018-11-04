914854ff353435687acac89fbd301201e3631fcf
Suma Dodmani <sumasdodmani@gmail.com>
2018-11-04 00:18:56 -0600

    Update containerWithMostWater.py

--- Arrays/containerWithMostWater.py
+++ Arrays/containerWithMostWater.py
@@ -1,1 +1,15 @@
-
+class Solution:
+    def maxArea(self, height):
+        """
+        :type height: List[int]
+        :rtype: int
+        """
+        left, right, area = 0, len(height)-1, 0
+        while (left < right):
+            if height[left] >= height[right]:
+                area = max(area,(right-left)*height[right])
+                right = right -1
+            else:
+                area = max(area, (right-left)*height[left])
+                left = left + 1
+        return area
