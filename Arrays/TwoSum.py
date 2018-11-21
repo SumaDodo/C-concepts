33aa57a612b06351123d2d68a0e95dc74ef27f55
Suma Dodmani <sumasdodmani@gmail.com>
2018-11-21 14:04:49 -0700

    Create TwoSum.py

---
+++ Arrays/TwoSum.py
@@ -0,0 +1,15 @@
+class Solution:
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        mapping = dict()
+        for index,numbers in enumerate(nums):
+            remaining = target - numbers
+            if remaining in mapping:
+                return [mapping[remaining],index]
+            else:
+                mapping[numbers] = index
+        
