84fc840d1b50318b54b06f31627a7550627f5114
Suma Dodmani <sumasdodmani@gmail.com>
2018-11-30 13:12:08 -0700

    Update vectorContainer.cpp

--- OOPS/vectorContainer.cpp
+++ OOPS/vectorContainer.cpp
@@ -1,1 +1,24 @@
+#include <iostream>
+#include <vector>
 
+int main() {
+
+    vector<int> vec;
+    int i;
+
+    std::cout << "before"<<vec.size()<<endl;
+
+    for (int i =0;i<5;i ++)
+    {
+        vec.push_back(i);
+    }
+
+    std::cout << "After size" << vec.size()<<endl;
+
+    //Using iterator to access the values
+    vector<int>::iterator v = vec.begin();
+    while(v !=vec.end()){
+        std::cout<<"value of v" << *v <<endl;
+    }
+    return 0;
+}
