a05e4b0dce5b75f44f4c76a45c6c7840a92b7fa7
Suma Dodmani <sumasdodmani@gmail.com>
2019-01-04 11:39:25 +0530

    Update longestSubstringWithoutRepitition.cpp

--- String/longestSubstringWithoutRepitition.cpp
+++ String/longestSubstringWithoutRepitition.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-        vector<int> dictionary(256,-1);
+        vector<int> dictionary(256,-1); //initialize the vector of length 256 all initialized to -1
         int start = -1, max_length=0;
         
         for (int i=0; i<s.length(); i++){
-            if(dictionary[s[i]]>start)
+            if(dictionary[s[i]]>start) //If there is repitition, then update the start to the index of the repitited char
                 start = dictionary[s[i]];
-            dictionary[s[i]]= i;
+            dictionary[s[i]]= i; // else update the vector with the char index
             max_length = max(max_length, i-start);
         }
         return max_length;
