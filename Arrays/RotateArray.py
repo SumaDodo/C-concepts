ede0212b7cbfd76403f4732ed9449e1c834c9aea
Suma Dodmani <sumasdodmani@gmail.com>
2019-01-11 11:47:00 +0530

    Update RotateArray.py

--- Arrays/RotateArray.py
+++ Arrays/RotateArray.py
@@ -1,3 +1,4 @@
+#Method 1: Time Limit Exceeded
 class Solution:
     def rotate(self, nums, k):
         """
@@ -13,4 +14,19 @@ class Solution:
                 temp = nums[j]
                 nums[j] = previous
                 previous = temp
+                
+#Method 2: Run tie 76ms. Time Complexity: O(n) space: O(n)
+class Solution:
+    def rotate(self, nums, k):
+        """
+        :type nums: List[int]
+        :type k: int
+        :rtype: void Do not return anything, modify nums in-place instead.
+        """
+        new = [None]*len(nums)
+        for i in range(len(nums)):
+            new[(i+k)%len(nums)] = nums[i]
+            
+        for i in range(len(nums)):
+            nums[i] = new[i]
                 
