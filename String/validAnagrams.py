6829cf21b6db2ce18b79d2429a0d8f213fa14a49
Suma Dodmani <sumasdodmani@gmail.com>
2019-02-19 20:26:14 -0700

    Update validAnagrams.py

--- String/validAnagrams.py
+++ String/validAnagrams.py
@@ -13,3 +13,19 @@ class Solution:
             return True
         else:
             return False
+        
+#Method 2:
+from collections import Counter
+
+class Solution:
+    def isAnagram(self, s: 'str', t: 'str') -> 'bool':
+        counter_s = Counter(s)
+        counter_t = Counter(t)
+        
+        if counter_s == counter_t:
+            return True
+        else:
+            return False
+        
+        
+        
